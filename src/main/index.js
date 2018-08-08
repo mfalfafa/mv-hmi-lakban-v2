a42531033cb65a0d40ad37dad042f9f5a986ade0
FF077 <miftahf77@gmail.com>
2018-08-08 13:48:30 +0700

    MV HMI Lakban v2

--- src/main/index.js
+++ src/main/index.js
@@ -20,7 +20,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 480,
     useContentSize: true,
-    width: 720,
+    width: 800,
     fullscreen: true,
     frame: false
   })

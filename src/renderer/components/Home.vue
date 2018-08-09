e2fa905bfd5e35b726e037d21d135abdc263952e
FF077 <miftahf77@gmail.com>
2018-08-09 17:21:24 +0700

    MV HMI Lakban v2

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -215,12 +215,14 @@ export default {
                     console.log("Error while sending datalakban!");
                     client.destroy();
                     f=0;
+                    connect_();
                     // Trying to reconnect
                     // connect_();
                 }
             }else if(f==0){
                 console.log("Client is not connected!!!");
                 client.destroy();
+                connect_();
             }
         }, 1000);
 
@@ -232,6 +234,7 @@ export default {
             n=0;
             //Close the connection
             client.destroy();
+            connect_();
         }
 
         client.on('error', function(err){
@@ -239,6 +242,7 @@ export default {
             console.log(err.toString());
             client.destroy();
             f=0;
+            connect_();
         });
 
         client.on('data', function(data) {
@@ -254,6 +258,7 @@ export default {
                 client.destroy();
             }
             f=0;
+            connect_();
           }
         });
 

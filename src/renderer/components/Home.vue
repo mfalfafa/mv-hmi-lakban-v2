23a4502151099bd5eb9f8deae228ddeb52800f0f
FF077 <miftahf77@gmail.com>
2018-08-09 17:42:41 +0700

    MV HMI Lakban v2

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -185,7 +185,6 @@ export default {
                 }catch(err){
                     console.log("Can't connect to server!");
                     client.destroy();
-                    connect_();
                     f=0;
                 }
             }
@@ -216,14 +215,12 @@ export default {
                     console.log("Error while sending datalakban!");
                     client.destroy();
                     f=0;
-                    connect_();
                     // Trying to reconnect
                     // connect_();
                 }
             }else if(f==0){
                 console.log("Client is not connected!!!");
                 client.destroy();
-                connect_();
             }
         }, 1000);
 
@@ -235,7 +232,6 @@ export default {
             n=0;
             //Close the connection
             client.destroy();
-            connect_();
         }
 
         client.on('error', function(err){
@@ -243,7 +239,6 @@ export default {
             console.log(err.toString());
             client.destroy();
             f=0;
-            connect_();
         });
 
         client.on('data', function(data) {
@@ -259,7 +254,6 @@ export default {
                 client.destroy();
             }
             f=0;
-            connect_();
           }
         });
 
